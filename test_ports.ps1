991751338127c26164c94bca7655b4c961db36f7
Jose Rueda <jose.rueda.luque@outlook.es>
2022-05-21 11:55:56 +0200

    Powershell example

---
+++ test_ports.ps1
@@ -0,0 +1,53 @@
+foreach ($pc in $listado){
+
+    $resultado = Test-Connection $pc -count 1 -quiet
+    
+    if ($resultado){
+    
+        $ComputerInfoObj = [PSCustomObject]@{
+    
+            Equipo = $pc
+    
+               # Comprobamos puertos
+            Puerto135 = Test-NetConnection $pc -Port 135 -ErrorAction SilentlyContinue | Select-Object -ExpandProperty TcpTestSucceeded
+            Puerto139 = Test-NetConnection $pc -Port 139 -ErrorAction SilentlyContinue | Select-Object -ExpandProperty TcpTestSucceeded
+            Puerto445 = Test-NetConnection $pc -Port 445 -ErrorAction SilentlyContinue | Select-Object -ExpandProperty TcpTestSucceeded
+
+
+            # Comprobamos servicios
+            ServerService = Get-Service -ComputerName $pc -Name LanmanServer | Select-Object -ExpandProperty Status
+            RemoteRPC = Get-Service -ComputerName $pc -Name RpcSS | Select-Object -ExpandProperty Status
+            RemoteRegistry = Get-Service -ComputerName $pc -Name RemoteRegistry | Select-Object -ExpandProperty Status
+            Wmi = Get-Service -ComputerName $pc -Name Winmgmt | Select-Object -ExpandProperty Status
+            Firewall = Get-Service -ComputerName $pc -Name MpsSvc | Select-Object -ExpandProperty Status
+        
+        }
+
+        $ComputerInfoObj
+
+    } else {
+
+        $ComputerInfoObj = [PSCustomObject]@{
+    
+            Equipo = $pc
+    
+               # Comprobamos puertos
+            Puerto135 = "Sin conexion"
+            Puerto139 = "Sin conexion"
+            Puerto445 = "Sin conexion"
+
+
+            # Comprobamos servicios
+            ServerService = "Sin conexion"
+            RemoteRPC = "Sin conexion"
+            RemoteRegistry = "Sin conexion"
+            Wmi = "Sin conexion"
+            Firewall = "Sin conexion"
+    }
+       
+        $ComputerInfoObj
+       
+
+    }
+
+}
